5a1353a5ff74a7fb86b670b494ecb16f2f75ff78
ceddddy <ceddddyadvance@gmail.com>
2022-02-09 06:25:34 +0200

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+//CSS STYLES
+//navigation bar
+const navBar = document.getElementById("navigationBar").style.cssText = `
+display: flex;
+font-size: 20px;
+font-family: cursive;
+justify-content: space-between`;
+
+//h1 element 
+const heading1 = document.getElementById("headingOne").style.cssText = `
+font-family:cursive;
+font-size: 50px;
+display: flex;
+justify-content: center;
+color: teal`;
+
